41b82ab0eb81f0b637af6ef213a2455ef6c76d48
Hemanth <90907017+hemanthnaika@users.noreply.github.com>
2021-12-19 21:30:56 +0530

    Add the product and Category

---
+++ src/actions/category.js
@@ -0,0 +1,34 @@
+
+import jwt from 'jsonwebtoken'
+import axios from 'axios'
+import { toast } from 'react-hot-toast'
+
+
+
+export const addCategory = (name, description) => async (dispatch) => {
+
+    try {
+        const base_Url = 'https://hemanth-e-comerce-api.herokuapp.com'
+
+        const res = await axios.post(`${base_Url}/api/v1/category/add`, {
+            name, description
+        })
+        console.log(res)
+        const { category, message } = res.data
+
+        if (category) {
+            toast.success(message)
+            dispatch({
+                type: "ADD_CATEGORY",
+            })
+        } else {
+            toast.error(message)
+            dispatch({
+                type: "ADD_CATEGORY_FAILED",
+            })
+        }
+    } catch (error) {
+        console.log(error.message)
+        toast.error(error.message)
+    }
+};
